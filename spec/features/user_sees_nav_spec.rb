bd72da7e9933d89e15ddfd3622b3a9d244ddc7f1
Daniel Briechle <danbriechle@hotmail.com>
2019-04-15 21:33:31 -0600

    Adds nav bar feature test

---
+++ spec/features/user_sees_nav_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  context 'when i visit my dahsboard' do
+    it 'sees nav bar' do
+      user_1 = User.create!(name: "Dan Briechle")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit dashboard_path
+      expect(page).to have_button("Projects")
+      expect(page).to have_button("Search")
+      expect(page).to have_field("q")
+    end
+  end
+end
