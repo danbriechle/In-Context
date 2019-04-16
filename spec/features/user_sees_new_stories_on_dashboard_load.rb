c299209dcdbd0cc76c61ed8e9c336aff73e73a5b
Daniel Briechle <danbriechle@hotmail.com>
2019-04-16 13:16:59 -0600

    Adds initial article prepop feature test

---
+++ spec/features/user_sees_new_stories_on_dashboard_load.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'as a user' do
+  context 'when i visit my dahsboard' do
+    it 'sees 10 prepopulated articles based upon my search tags' do
+      user_1 = User.create!(name: "Dan Briechle")
+      project_1 = Project.create!(title: "Look at how much this cost",
+                                  description: "Rural america from the outsider's perspective",
+                                  media: "mixed",
+                                  due_date: "Feb 6th",
+                                  search_tag: "rural america",
+                                  user: user_1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit "/"
+
+      within ".Article-1" do
+      expect(page).to have_content("Title")
+      end
+    end
+  end
+end
