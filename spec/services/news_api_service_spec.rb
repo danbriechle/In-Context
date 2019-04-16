391bb54d41ed46104a639d947414b336244405f9
Daniel Briechle <danbriechle@hotmail.com>
2019-04-16 11:03:20 -0600

    Adds correct key search to news service spec

--- spec/services/news_api_service_spec.rb
+++ spec/services/news_api_service_spec.rb
@@ -4,7 +4,7 @@ describe 'News API ' do
   it 'can connect' do
     search_term = "Fast Fashion"
     response = NewsApiService.stories(search_term)
-    keys = [:source, :name, :author, :title, :description, :urlToImage, :content]
+    keys = [:source, :author, :title, :description, :url, :urlToImage, :publishedAt, :content]
     expect(response[:articles][0].keys).to eq(keys)
   end
 end
