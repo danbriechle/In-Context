05ec1ffc18cdd8915828e526e6abacf74e4488db
Daniel Briechle <danbriechle@hotmail.com>
2019-04-16 14:11:34 -0600

    Refactors article list poro

--- app/poros/article_list.rb
+++ app/poros/article_list.rb
@@ -1,29 +1,12 @@
 class ArticleList
-  attr_reader :list
-  def initialize
-    @list = []
-  end
 
-
-  def add_article(article)
-    @list << article
-  end
-
-  def update
-    get_stories.each do |article|
-      add_article(article)
-    end
-  end
-
-  def get_stories
+  def self.update
     current_user_search_tags.map do |tag|
       NewsApiService.stories(tag)
-    end
+    end.first
   end
 
-  private
-
-  def current_user_search_tags
+  def self.current_user_search_tags
     @current_user ||= User.last.projects.map do |project|
       project[:search_tag]
     end
--- spec/poros/article_list_poro_spec.rb
+++ spec/poros/article_list_poro_spec.rb
@@ -1,48 +1,8 @@
 RSpec.describe ArticleList, type: :poro do
   describe "Class Methods" do
-    describe ".add_article" do
-      it "can have articles added to it" do
-        articlelist_1 = ArticleList.new
-        user_1 = User.create!(name: "Dan Briechle")
-        project_1 = Project.create!(title: "Look at how much this cost",
-                                    description: "Rural america from the outsider's perspective",
-                                    media: "mixed",
-                                    due_date: "Feb 6th",
-                                    search_tag: "rural america",
-                                    user: user_1)
-        article_1 = Article.create!(title: "Getting Real About Rural America",
-                                    authors: "PAUL KRUGMAN",
-                                    source: "The New York Times",
-                                    image_url: "https://static01.nyt.com/images/2019/03/18/opinion/18krugmanWeb/18krugmanWeb-facebookJumbo.jpg",
-                                    publication_date: "2019-03-18T22:00:06Z",
-                                    description: "Politically, rural America is increasingly a world apart.",
-                                    project: project_1,
-                                    content: "Even then, rural areas and small towns werent the real America, somehow morally
-                                    superior to the rest of us. But they were a major part of the demographic, social
-                                    and cultural landscape.Since then, however, while Americas population has doubled,
-                                    the number o… ")
-        article_2 = Article.create!(title: "What Rural America Has to Teach Us",
-                                    authors: "DAVID BROOKS",
-                                    source: "The New York Times",
-                                    image_url: "https://static01.nyt.com/images/2019/03/21/opinion/21brooks1/21brooks1-facebookJumbo.jpg",
-                                    publication_date: "2019-03-21T22:33:22Z",
-                                    description: "A couple at a “cowboy church” service at the Nebraska State Fair in Grand Island.",
-                                    project: project_1,
-                                    content: "Even then, rural areas and small towns werent the real America, somehow morally
-                                    superior to the rest of us. But they were a major part of the demographic, social
-                                    and cultural landscape.Since then, however, while Americas population has doubled,
-                                    the number o… ")
-
-        articlelist_1.add_article(article_1)
-
-        expect(articlelist_1.list[0][:title]).to eq("Getting Real About Rural America")
-        articlelist_1.add_article(article_2)
-        expect(articlelist_1.list[1][:title]).to eq("What Rural America Has to Teach Us")
-        expect(articlelist_1.list.count).to eq(2)
-      end
-
+    describe ".update" do
       it 'can be updated with curent_user project search tags' do
-        articlelist_1 = ArticleList.new
+
         user_1 = User.create!(name: "Dan Briechle")
         project_1 = Project.create!(title: "Look at how much this cost",
                                     description: "Rural america from the outsider's perspective",
@@ -53,9 +13,10 @@ RSpec.describe ArticleList, type: :poro do
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-        articlelist_1.update
+        list = ArticleList.update
+        
 
-        expect(articlelist_1.list[0].count).to eq(5)
+        expect(list.count).to eq(5)
       end
     end
   end
